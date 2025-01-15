b60d274f37bf1216f1f09a6e9e1dc68e8611e3f0
Yiannis Marangos <yiannis@eiger.co>
2025-01-15 13:52:35 +0200

    wip

---
+++ devenv.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    pkgs.protobuf
+    pkgs.wasm-pack
+  ];
+
+  cachix.enable = false;
+  dotenv.enable = true;
+
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    # Extra targets other than the native
+    targets = ["wasm32-unknown-unknown"];
+  };
+
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
+
+  languages.swift.enable = true;
+  languages.kotlin.enable = true;
+}
